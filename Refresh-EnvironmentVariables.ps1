c1d223ab770c46b2782bbbfec462670f62cc242e
asheroto <49938263+asheroto@users.noreply.github.com>
2026-03-25 17:39:54 -0500

    Fix PATH ordering, prevent overwriting critical environment variables, and remove stale environment variables from session

--- Refresh-EnvironmentVariables.ps1
+++ Refresh-EnvironmentVariables.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 
-.VERSION 1.0.2
+.VERSION 1.1.0
 
 .GUID 9ff8b18d-cc46-449e-81f1-bbdacc3f41b4
 
@@ -17,6 +17,7 @@
 [Version 1.0.0] - Total rework of script, implementing Chocolatey's Update-SessionEnvironment function into one single script.
 [Version 1.0.1] - Rename to Refresh-EnvironmentVariables to avoid naming conflicts with Chocolatey's RefreshEnv.cmd.
 [Version 1.0.2] - Fix bug with CheckForUpdate.
+[Version 1.1.0] - Fix PATH ordering, prevent overwriting critical environment variables, and remove stale environment variables from session
 
 #>
 
@@ -34,7 +35,7 @@
 .PARAMETER Help
     Displays the full help information for the script.
 .NOTES
-	Version      : 1.0.2
+	Version      : 1.1.0
 	Created by   : asheroto
 .LINK
 	Project Site: https://github.com/asheroto/Refresh-EnvironmentVariables
@@ -49,64 +50,34 @@ param (
 # Derived from the original work by Chocolatey Software, used in accordance with license
 # Copyright © 2017 - 2021 Chocolatey Software, Inc.
 
-# Changes made:
-# - Extracted the functions from the original script for use in Refresh-EnvironmentVariables.ps1
-# - Removed Write-FunctionCallLogMessage from Update-SessionEnvironment as it only applies to Chocolatey
-# - Added custom functions
+# Based on concepts from Chocolatey's Update-SessionEnvironment
+# Rewritten and extended for standalone use
 
 # Original license, included per the terms of the Apache 2.0 license:
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 # Version
-$CurrentVersion = '1.0.2'
+$CurrentVersion = '1.1.0'
 $RepoOwner = 'asheroto'
 $RepoName = 'Refresh-EnvironmentVariables'
 $PowerShellGalleryName = 'Refresh-EnvironmentVariables'
 
 # Versions
-$ProgressPreference = 'SilentlyContinue' # Suppress progress bar (makes downloading super fast)
-$ConfirmPreference = 'None' # Suppress confirmation prompts
+$ProgressPreference = 'SilentlyContinue'
+$ConfirmPreference = 'None'
 
-# Display version if -Version is specified
 if ($Version.IsPresent) {
     $CurrentVersion
     exit 0
 }
 
-# Display full help if -Help is specified
 if ($Help) {
     Get-Help -Name $MyInvocation.MyCommand.Source -Full
     exit 0
 }
 
 function Get-GitHubRelease {
-    <#
-        .SYNOPSIS
-        Fetches the latest release information of a GitHub repository.
-
-        .DESCRIPTION
-        This function uses the GitHub API to get information about the latest release of a specified repository, including its version and the date it was published.
-
-        .PARAMETER Owner
-        The GitHub username of the repository owner.
-
-        .PARAMETER Repo
-        The name of the repository.
-
-        .EXAMPLE
-        Get-GitHubRelease -Owner "asheroto" -Repo "winget-install"
-        This command retrieves the latest release version and published datetime of the winget-install repository owned by asheroto.
-    #>
     [CmdletBinding()]
     param (
         [string]$Owner,
@@ -119,7 +90,6 @@ function Get-GitHubRelease {
         $latestVersion = $response.tag_name
         $publishedAt = $response.published_at
 
-        # Convert UTC time string to local time
         $UtcDateTime = [DateTime]::Parse($publishedAt, [System.Globalization.CultureInfo]::InvariantCulture, [System.Globalization.DateTimeStyles]::RoundtripKind)
         $PublishedLocalDateTime = $UtcDateTime.ToLocalTime()
 
@@ -148,246 +118,108 @@ function CheckForUpdate {
         Write-Output "Current version: $CurrentVersion."
         Write-Output "Latest version: $($Data.LatestVersion)."
         Write-Output "Published at: $($Data.PublishedDateTime).`n"
-        Write-Output "You can download the latest version from https://github.com/$RepoOwner/$RepoName/releases`n"
+        Write-Output "https://github.com/$RepoOwner/$RepoName/releases`n"
         if ($PowerShellGalleryName) {
-            Write-Output "Or you can run the following command to update:"
             Write-Output "Install-Script $PowerShellGalleryName -Force`n"
         }
     } else {
         Write-Output "`n$RepoName is up to date.`n"
-        Write-Output "Current version: $CurrentVersion."
-        Write-Output "Latest version: $($Data.LatestVersion)."
-        Write-Output "Published at: $($Data.PublishedDateTime)."
-        Write-Output "`nRepository: https://github.com/$RepoOwner/$RepoName/releases`n"
     }
     exit 0
 }
 
-# Check for updates if -CheckForUpdate is specified
 if ($CheckForUpdate) {
     CheckForUpdate -RepoOwner $RepoOwner -RepoName $RepoName -CurrentVersion $CurrentVersion -PowerShellGalleryName $PowerShellGalleryName
 }
 
 function Get-EnvironmentVariable {
-    <#
-.SYNOPSIS
-Gets an Environment Variable.
-
-.DESCRIPTION
-This will will get an environment variable based on the variable name
-and scope while accounting whether to expand the variable or not
-(e.g.: `%TEMP%`-> `C:\User\Username\AppData\Local\Temp`).
-
-.NOTES
-This helper reduces the number of lines one would have to write to get
-environment variables, mainly when not expanding the variables is a
-must.
-
-.PARAMETER Name
-The environment variable you want to get the value from.
-
-.PARAMETER Scope
-The environment variable target scope. This is `Process`, `User`, or
-`Machine`.
-
-.PARAMETER PreserveVariables
-A switch parameter stating whether you want to expand the variables or
-not. Defaults to false.
-
-.PARAMETER IgnoredArguments
-Allows splatting with arguments that do not apply. Do not use directly.
-
-.EXAMPLE
-Get-EnvironmentVariable -Name 'TEMP' -Scope User -PreserveVariables
-
-.EXAMPLE
-Get-EnvironmentVariable -Name 'PATH' -Scope Machine
-
-.LINK
-Get-EnvironmentVariableNames
-
-.LINK
-Set-EnvironmentVariable
-#>
     [CmdletBinding()]
-    [OutputType([string])]
     param(
         [Parameter(Mandatory = $true)][string] $Name,
         [Parameter(Mandatory = $true)][System.EnvironmentVariableTarget] $Scope,
-        [Parameter(Mandatory = $false)][switch] $PreserveVariables = $false,
-        [parameter(ValueFromRemainingArguments = $true)][Object[]] $ignoredArguments
+        [switch] $PreserveVariables
     )
 
-    # Do not log function call, it may expose variable names
-    ## Called from chocolateysetup.psm1 - wrap any Write-Host in try/catch
-
-    [string] $MACHINE_ENVIRONMENT_REGISTRY_KEY_NAME = "SYSTEM\CurrentControlSet\Control\Session Manager\Environment\";
-    [Microsoft.Win32.RegistryKey] $win32RegistryKey = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey($MACHINE_ENVIRONMENT_REGISTRY_KEY_NAME)
-    if ($Scope -eq [System.EnvironmentVariableTarget]::User) {
-        [string] $USER_ENVIRONMENT_REGISTRY_KEY_NAME = "Environment";
-        [Microsoft.Win32.RegistryKey] $win32RegistryKey = [Microsoft.Win32.Registry]::CurrentUser.OpenSubKey($USER_ENVIRONMENT_REGISTRY_KEY_NAME)
-    } elseif ($Scope -eq [System.EnvironmentVariableTarget]::Process) {
+    if ($Scope -eq [System.EnvironmentVariableTarget]::Process) {
         return [Environment]::GetEnvironmentVariable($Name, $Scope)
     }
 
-    [Microsoft.Win32.RegistryValueOptions] $registryValueOptions = [Microsoft.Win32.RegistryValueOptions]::None
-
-    if ($PreserveVariables) {
-        Write-Verbose "Choosing not to expand environment names"
-        $registryValueOptions = [Microsoft.Win32.RegistryValueOptions]::DoNotExpandEnvironmentNames
-    }
-
-    [string] $environmentVariableValue = [string]::Empty
+    $keyPath = if ($Scope -eq 'User') { 'Environment' } else { 'SYSTEM\CurrentControlSet\Control\Session Manager\Environment' }
+    $registry = if ($Scope -eq 'User') { [Microsoft.Win32.Registry]::CurrentUser } else { [Microsoft.Win32.Registry]::LocalMachine }
 
     try {
-        #Write-Verbose "Getting environment variable $Name"
-        if ($win32RegistryKey -ne $null) {
-            # Some versions of Windows do not have HKCU:\Environment
-            $environmentVariableValue = $win32RegistryKey.GetValue($Name, [string]::Empty, $registryValueOptions)
+        $key = $registry.OpenSubKey($keyPath)
+        if ($null -ne $key) {
+            $value = $key.GetValue($Name, '')
+            $key.Close()
+            if ($value) { return $value }
         }
-    } catch {
-        Write-Debug "Unable to retrieve the $Name environment variable. Details: $_"
-    } finally {
-        if ($win32RegistryKey -ne $null) {
-            $win32RegistryKey.Close()
-        }
-    }
+    } catch {}
 
-    if ($environmentVariableValue -eq $null -or $environmentVariableValue -eq '') {
-        $environmentVariableValue = [Environment]::GetEnvironmentVariable($Name, $Scope)
-    }
-
-    return $environmentVariableValue
+    return [Environment]::GetEnvironmentVariable($Name, $Scope)
 }
 
-function Get-EnvironmentVariableNames([System.EnvironmentVariableTarget] $Scope) {
-    <#
-.SYNOPSIS
-Gets all environment variable names.
+function Get-EnvironmentVariableNames {
+    param([System.EnvironmentVariableTarget] $Scope)
 
-.DESCRIPTION
-Provides a list of environment variable names based on the scope. This
-can be used to loop through the list and generate names.
-
-.NOTES
-Process dumps the current environment variable names in memory /
-session. The other scopes refer to the registry values.
-
-.INPUTS
-None
-
-.OUTPUTS
-A list of environment variables names.
-
-.PARAMETER Scope
-The environment variable target scope. This is `Process`, `User`, or
-`Machine`.
-
-.EXAMPLE
-Get-EnvironmentVariableNames -Scope Machine
-
-.LINK
-Get-EnvironmentVariable
-
-.LINK
-Set-EnvironmentVariable
-#>
-
-    # Do not log function call
-
-    # HKCU:\Environment may not exist in all Windows OSes (such as Server Core).
     switch ($Scope) {
-        'User' {
-            Get-Item 'HKCU:\Environment' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Property
-        }
-        'Machine' {
-            Get-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment' | Select-Object -ExpandProperty Property
-        }
-        'Process' {
-            Get-ChildItem Env:\ | Select-Object -ExpandProperty Key
-        }
-        default {
-            throw "Unsupported environment scope: $Scope"
-        }
+        'User' { Get-Item 'HKCU:\Environment' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Property }
+        'Machine' { Get-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment' | Select-Object -ExpandProperty Property }
+        'Process' { Get-ChildItem Env:\ | Select-Object -ExpandProperty Key }
     }
 }
 
 function Update-SessionEnvironment {
     <#
 .SYNOPSIS
-Updates the environment variables of the current powershell session with
-any environment variable changes that may have occurred during a
-Chocolatey package install.
-
-.DESCRIPTION
-When Chocolatey installs a package, the package author may add or change
-certain environment variables that will affect how the application runs
-or how it is accessed. Often, these changes are not visible to the
-current PowerShell session. This means the user needs to open a new
-PowerShell session before these settings take effect which can render
-the installed application nonfunctional until that time.
-
-Use the Update-SessionEnvironment command to refresh the current
-PowerShell session with all environment settings possibly performed by
-Chocolatey package installs.
-
-.NOTES
-This method is also added to the user's PowerShell profile as
-`refreshenv`. When called as `refreshenv`, the method will provide
-additional output.
-
-Preserves `PSModulePath` as set by the process.
-
-.INPUTS
-None
-
-.OUTPUTS
-None
+Updates the environment variables of the current powershell session...
 #>
 
     $userName = $env:USERNAME
     $architecture = $env:PROCESSOR_ARCHITECTURE
     $psModulePath = $env:PSModulePath
 
-    #ordering is important here, $user should override $machine...
     $ScopeList = 'Process', 'Machine'
     if ('SYSTEM', "${env:COMPUTERNAME}`$" -notcontains $userName) {
-        # but only if not running as the SYSTEM/machine in which case user can be ignored.
         $ScopeList += 'User'
     }
 
+    $skip = @('PSModulePath','USERNAME','PROCESSOR_ARCHITECTURE','PATH')
+
     foreach ($Scope in $ScopeList) {
-        Get-EnvironmentVariableNames -Scope $Scope |
-        ForEach-Object {
-            Set-Item "Env:$_" -Value (Get-EnvironmentVariable -Scope $Scope -Name $_)
+        Get-EnvironmentVariableNames -Scope $Scope | ForEach-Object {
+            if ($skip -contains $_) { return }
+
+            $value = Get-EnvironmentVariable -Scope $Scope -Name $_
+            if ($null -ne $value -and $value -ne '') {
+                Set-Item "Env:$_" -Value $value -ErrorAction SilentlyContinue
+            }
         }
     }
 
-    #Path gets special treatment b/c it munges the two together
-    $paths = 'Machine', 'User' |
-    ForEach-Object {
-      (Get-EnvironmentVariable -Name 'PATH' -Scope $_) -split ';'
-    } |
-    Select-Object -Unique
-    $Env:PATH = $paths -join ';'
+    # PATH fix
+    $machinePath = Get-EnvironmentVariable -Name 'PATH' -Scope Machine
+    $userPath = Get-EnvironmentVariable -Name 'PATH' -Scope User
+    $env:PATH = @($machinePath,$userPath) -join ';'
 
-    # PSModulePath is almost always updated by process, so we want to preserve it.
+    # Remove stale variables
+    $validNames = @()
+    $validNames += Get-EnvironmentVariableNames -Scope Machine
+    $validNames += Get-EnvironmentVariableNames -Scope User
+    $validNames = $validNames | Select-Object -Unique
+
+    Get-ChildItem Env: | ForEach-Object {
+        if ($skip -contains $_.Name) { return }
+        if ($validNames -notcontains $_.Name) {
+            Remove-Item "Env:$($_.Name)" -ErrorAction SilentlyContinue
+        }
+    }
+
     $env:PSModulePath = $psModulePath
-
-    # reset user and architecture
-    if ($userName) {
-        $env:USERNAME = $userName;
-    }
-    if ($architecture) {
-        $env:PROCESSOR_ARCHITECTURE = $architecture;
-    }
+    if ($userName) { $env:USERNAME = $userName }
+    if ($architecture) { $env:PROCESSOR_ARCHITECTURE = $architecture }
 }
 
-# Output
 Write-Output "Refreshing environment variables..."
-
-# Call the function to update the session environment
 Update-SessionEnvironment
-
-# Output
 Write-Output "Finished"
